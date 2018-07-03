3a59029ea520970a468e934b84236788834f6c2a
vincent-lee <laomumeme@gmail.com>
2018-07-03 20:01:42 +0800

    1

--- app/Http/Controllers/Chat/AjaxStatusController.php
+++ app/Http/Controllers/Chat/AjaxStatusController.php
@@ -12,7 +12,7 @@ class AjaxStatusController extends Controller
     //在线人数状态
     public function online()
     {
-        Redis::select(2);
+        Redis::select(1);
         $key = 'online';
         //还没检查在线状态的人数
         $onlineNum =  Redis::LLEN($key);
@@ -32,7 +32,7 @@ class AjaxStatusController extends Controller
     //检查此人在线状态
     public function getOnlineStatus(Request $request)
     {
-        Redis::select(2);
+        Redis::select(1);
         $userid = $request->get('id');
 
         //检查在线状态

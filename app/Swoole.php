c8142d7ca154e5fbc78fb08e712bca53f5bef51c
laomumeme <laomumeme@gmail.com>
2018-09-12 04:01:55 +0800

    modify swoole

--- app/Swoole.php
+++ app/Swoole.php
@@ -14,6 +14,7 @@ class Swoole
     private function postSwoole($param){
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, env('WS_CURL',"http://127.0.0.1")."/dows");
+        echo env('WS_CURL',"http://127.0.0.1")."/dows";
         //设置post数据
         curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
         curl_setopt($ch,CURLOPT_POST,1);

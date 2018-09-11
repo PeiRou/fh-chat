6ec638f98388630704cec17395868f3fe5a05aed
laomumeme <laomumeme@gmail.com>
2018-09-12 02:16:11 +0800

    modify swoole

--- config/database.php
+++ config/database.php
@@ -61,7 +61,7 @@ return [
             'write' => [
                 'host' => env('DB_WRITE_HOST_1')
             ],
-            //'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),

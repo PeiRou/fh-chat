22446946c3d8f3887503cb44c8b545e7b893a99f
laomumeme <laomumeme@gmail.com>
2018-09-12 04:17:49 +0800

    modify swoole

--- app/Swoole.php
+++ app/Swoole.php
@@ -12,14 +12,17 @@ class Swoole
         return $this->postSwoole($param);
     }
     private function postSwoole($param){
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, env('WS_CURL',"http://127.0.0.1").":".env('WS_PORT',9500));
-        //设置post数据
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-        curl_setopt($ch,CURLOPT_POST,1);
-        curl_setopt($ch,CURLOPT_POSTFIELDS,$param);
-        $output = curl_exec($ch);
-        curl_close($ch);
-        return $output;
+//        $ch = curl_init();
+//        curl_setopt($ch, CURLOPT_URL, env('WS_CURL',"http://127.0.0.1").":".env('WS_PORT',9500));
+//        //设置post数据
+//        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+//        curl_setopt($ch,CURLOPT_POST,1);
+//        curl_setopt($ch,CURLOPT_POSTFIELDS,$param);
+//        $output = curl_exec($ch);
+//        curl_close($ch);
+        $http = new \GuzzleHttp\Client();
+        $res = $http->request('POST',env('WS_CURL',"http://127.0.0.1").":".env('WS_PORT',9500)."?type=".$param['type']."&room=".$param['type']);
+        $json = json_decode((string) $res->getBody(), true);
+        return $json;
     }
 }

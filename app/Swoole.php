2bbdfcb9c6b52f8347641fee119938870afdfa0e
“Zoe” <phapha0513@gmail.com>
2018-09-13 13:58:56 +0800

    modify

--- app/Swoole.php
+++ app/Swoole.php
@@ -14,16 +14,20 @@ class Swoole
     }
     private function postSwoole($param){
 //        $ch = curl_init();
-//        curl_setopt($ch, CURLOPT_URL, env('WS_CURL',"http://127.0.0.1").":".env('WS_PORT',9500));
-//        //设置post数据
-//        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-//        curl_setopt($ch,CURLOPT_POST,1);
-//        curl_setopt($ch,CURLOPT_POSTFIELDS,$param);
-//        $output = curl_exec($ch);
-//        curl_close($ch);
-        $http = new Client();
-        $res = $http->request('POST',env('WS_CURL',"http://127.0.0.1").":".env('WS_PORT',9500)."?type=".$param['type']."&room=".$param['type']);
-        $json = json_decode((string) $res->getBody(), true);
+        $this->ch = curl_init();
+        //设置post数据
+        curl_setopt($this->ch,CURLOPT_URL,env('WS_CURL',"http://127.0.0.1").":".env('WS_PORT',9500));
+        curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书检查
+        curl_setopt($this->ch,CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($this->ch,CURLOPT_POST,1);
+        curl_setopt($this->ch,CURLOPT_POSTFIELDS,$param);
+        curl_setopt($this->ch, CURLOPT_TIMEOUT, $this->timeout);
+        $output = curl_exec($this->ch);
+        return $output;
+        curl_close($ch);
+//        $http = new Client();
+//        $res = $http->request('POST',env('WS_CURL',"http://127.0.0.1").":".env('WS_PORT',9500)."?type=".$param['type']."&room=".$param['type']);
+//        $json = json_decode((string) $res->getBody(), true);
         return $json;
     }
 }

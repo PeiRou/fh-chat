230feb66be065ccd6d131296156288152b30eda6
“Zoe” <phapha0513@gmail.com>
2018-10-26 00:58:06 +0800

    modify hb time out

--- app/Swoole.php
+++ app/Swoole.php
@@ -21,10 +21,10 @@ class Swoole
         curl_setopt($this->ch,CURLOPT_RETURNTRANSFER,1);
         curl_setopt($this->ch,CURLOPT_POST,1);
         curl_setopt($this->ch,CURLOPT_POSTFIELDS,$data);
-        curl_setopt($this->ch, CURLOPT_TIMEOUT, $this->timeout);
+//        curl_setopt($this->ch, CURLOPT_TIMEOUT, $this->timeout);
         $redis = Redis::connection();
         $redis->select(5);
-        $redis->setex('hbpost'.http_build_query($data),$this->timeout,'on');
+        $redis->setex('hbpost'.http_build_query($data),3,'on');
         $output = curl_exec($this->ch);
         if($redis->exists('hbpost'.http_build_query($data)))
             return 'ok';

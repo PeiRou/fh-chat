42b043e564dbac001bc89a057c1795dc5aa4915d
“Zoe” <phapha0513@gmail.com>
2018-11-04 14:57:06 +0800

    modify ws_cul

--- app/Swoole.php
+++ app/Swoole.php
@@ -15,9 +15,8 @@ class Swoole
     }
     private function postSwoole($param,$data=array()){
         $this->ch = curl_init();
-        echo env('WS_CURL',"https://127.0.0.1:2021");
         //设置post数据
-        curl_setopt($this->ch,CURLOPT_URL,env('WS_CURL',"https://127.0.0.1:2021")."/?type=".$param['type']."&room=".$param['room']);
+        curl_setopt($this->ch,CURLOPT_URL,env('WS_CURL',"http://127.0.0.1")."/dows/?type=".$param['type']."&room=".$param['room']);
         curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书检查
         curl_setopt($this->ch,CURLOPT_RETURNTRANSFER,1);
         curl_setopt($this->ch,CURLOPT_POST,1);

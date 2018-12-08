c21e1028bd78a5a5a7e1102ff63da38b9f289120
Ten <tentenhentai557@gmail.com>
2018-12-08 15:20:01 +0800

    聊天室红包

--- app/Http/Controllers/Chat/ChatSettingController.php
+++ app/Http/Controllers/Chat/ChatSettingController.php
@@ -277,7 +277,8 @@ class ChatSettingController extends Controller
         $planSendGameCQSSC = $request->input('planSendGameCQSSC');                  //计划推送游戏-重庆时时彩
         if($planSendGameCQSSC=="on")
             $data['plan_send_game'] .= (isset($data['plan_send_game'])?",":"")."1";
-        if($planSendGameCQSSC=="on")                                                //计划推送游戏-江苏快三
+        $planSendGameJSKS = $request->input('planSendGameJSKS');                  //计划推送游戏-重庆时时彩
+        if($planSendGameJSKS=="on")                                                //计划推送游戏-江苏快三
             $data['plan_send_game'] .= (isset($data['plan_send_game'])?",":"")."10";
         $data['plan_msg'] = $request->input('planMsg');                             //计划底部信息
         $data['send_starttime'] = $request->input('starttime');                     //发布时段(开始)

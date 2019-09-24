7fcd9220375ece9cac7d733d66a2680d9c1b099e
zoe <zoe@gmail.com>
2019-09-24 13:51:59 +0800

    聊天室刪減以及修复的功能

--- resources/views/chat/roleManage.blade.php
+++ resources/views/chat/roleManage.blade.php
@@ -12,9 +12,9 @@
             <thead>
                 <th width="16.66%">角色名</th>
                 <th width="16.66%">聊天信息效果</th>
-                <th width="16.66%">消息最大长度</th>
+                {{--<th width="16.66%">消息最大长度</th>--}}
                 <th width="16.66%">权限</th>
-                <th width="16.66%">描述</th>
+                {{--<th width="16.66%">描述</th>--}}
                 <th width="16.66%">操作</th>
             </tr>
             </thead>

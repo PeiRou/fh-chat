eece009a9914b163c824b42b07f3903f6125f567
xiaocui <194198606@qq.com>
2019-09-28 14:22:15 +0800

    置顶

--- app/Socket/Model/ChatBase.php
+++ app/Socket/Model/ChatBase.php
@@ -14,10 +14,10 @@ class ChatBase extends Base
 {
     protected static function getValue($db, $value, $param = [])
     {
-        return self::HandleCacheData(function()use($db, $param, $value){
+        return self::RedisCacheData(function()use($db, $param, $value){
             foreach ($param as $k=>$v)
                 $db->where($k, $v);
             return $db->getOne('chat_base', [$value])[$value] ?? null;
-        }, 5, true);
+        }, 30, true);
     }
 }
